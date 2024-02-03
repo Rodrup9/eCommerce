c6dc27fb62f33c901947c2ce5c167ceaa432b76a
Rodrigo Alvarez <rodrup9@gmail.com>
2024-02-02 20:58:25 -0600

    Creando la ruta, estilos, controlador y vista de Home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/home.css">
+    <link rel="stylesheet" href="/css/globals.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Home</title>
+</head>
+<body>
+    <header class="menuGeneral">
+        <nav class="menuPrincipal">
+            <div class="menuRight">
+                <i class="logo">
+                    
+                </i>
+                <div class="boxBuscador">
+                    <div class="lupa">
+                        <i class='bx bx-search'></i>
+                    </div>
+                    <input type="search" name="" class="buscador" id="" placeholder="Buscar">
+                </div>
+            </div>
+            <div class="menuLeft">
+                <a href="#" class="opcionPrincipal">Iniciar sesión</a>
+                <a href="#" class="opcionPrincipal">Registrarse</a>
+            </div>
+        </nav>
+        <nav class="menuSecundario">
+            <div class="menuHamburger">
+                <i class='bx bx-menu iconMenu'></i>
+            </div>
+            <div class="listaOption">
+                <a href="#" class="opcionSecundaria">Tecnologia</a>
+                <a href="#" class="opcionSecundaria">Hogar</a>
+                <a href="#" class="opcionSecundaria">Electrodomesticos</a>
+                <a href="#" class="opcionSecundaria">Video Juegos</a>
+                <a href="#" class="opcionSecundaria">Ropa</a>
+            </div>
+        </nav>
+    </header>
+
+    <main class="boxOfertas">
+        <div class="tittle"><p>Ofertas especiales</p> <a class="verMas" href="#">Ver más</a></div>
+        <div class="boxSliderOfertas">
+            <div class="boxBoxOfertas">
+                <div class="boxOfertaMain">
+                    <div class="oferta1">Oferta</div>
+                    <div class="oferta2">Oferta</div>
+                    <div class="oferta3">Oferta</div>
+                    <div class="oferta4">Oferta</div>
+                    <div class="oferta5">Oferta</div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <section class="section sugerencias">
+        <div class="tittle"><p>Busqiedas recientes</p> <a class="verMas" href="#">Ver más</a></div>
+        <div class="boxProductos">
+            <div class="arrow">
+                    <i class='bx bxs-chevron-left arrowIcon'></i>
+            </div>
+            <div class="sliderProductos">
+                <div class="sliderBoxProductos">
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem debitis quo harum dignissimos explicabo doloribus ut dolorem illo quas dolores.</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">OUBNM</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="arrow">
+                    <i class='bx bxs-chevron-right arrowIcon'></i>
+            </div>
+        </div>
+    </section>
+
+    <section class="section ">
+        <div class="tittle"><p>Sugerencias</p> <a class="verMas" href="#">Ver más</a></div>
+        <div class="boxProductos">
+            <div class="arrow">
+                    <i class='bx bxs-chevron-left arrowIcon'></i>
+            </div>
+            <div class="sliderProductos">
+                <div class="sliderBoxProductos">
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem debitis quo harum dignissimos explicabo doloribus ut dolorem illo quas dolores.</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">OUBNM</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="arrow">
+                    <i class='bx bxs-chevron-right arrowIcon'></i>
+            </div>
+        </div>
+    </section>
+
+    <section class="section ">
+        <div class="tittle"><p>Tendencias</p> <a class="verMas" href="#">Ver más</a></div>
+        <div class="boxProductos">
+            <div class="arrow">
+                    <i class='bx bxs-chevron-left arrowIcon'></i>
+            </div>
+            <div class="sliderProductos">
+                <div class="sliderBoxProductos">
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem debitis quo harum dignissimos explicabo doloribus ut dolorem illo quas dolores.</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">OUBNM</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="arrow">
+                    <i class='bx bxs-chevron-right arrowIcon'></i>
+            </div>
+        </div>
+    </section>
+
+    <section class="section ">
+        <div class="tittle"><p>No se</p> <a class="verMas" href="#">Ver más</a></div>
+        <div class="boxProductos">
+            <div class="arrow">
+                    <i class='bx bxs-chevron-left arrowIcon'></i>
+            </div>
+            <div class="sliderProductos">
+                <div class="sliderBoxProductos">
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem debitis quo harum dignissimos explicabo doloribus ut dolorem illo quas dolores.</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">OUBNM</p>
+                        </div>
+                    </div>
+                    <div class="card producto">
+                        <div class="imgProducto">
+                            <img src="/img/logo.jpeg" alt="">
+                        </div>
+                        <div class="detalles">
+                            <p class="precio">$350</p>
+                            <p class="descripcion">Descripcion breve</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="arrow">
+                    <i class='bx bxs-chevron-right arrowIcon'></i>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        
+    </footer>
+</body>
+</html>
