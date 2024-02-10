1167311f27e79928cd36166232c37273ee16b265
Leonardo <leo1.25daniel@gmail.com>
2024-02-10 12:29:24 -0600

    Creacion de rama y de la base de datos

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'v10.3.2',
-        'version' => '10.3.2.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b75b5fce82720bda056794c88ef0f12a3ecde129',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -380,9 +380,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'v10.3.2',
-            'version' => '10.3.2.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b75b5fce82720bda056794c88ef0f12a3ecde129',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
